d0579c674ea76e59c2ef814c5a95f652147a95da
Gianluca Di Maria <gianlucadimariayt@gmail.com>
2025-11-05 14:00:21 +0000

    calculated sales_margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,26 @@
+with 
+
+sales as (
+    select * from {{ ref('stg_raw__sales') }}
+),
+
+products as (
+    select * from {{ ref('stg_raw__product') }}
+),
+
+joined as (
+    select
+        sales.orders_id,
+        sales.products_id,
+        sales.quantity,
+        sales.revenue,
+        products.purchase_price,
+        sales.quantity * products.purchase_price as purchase_cost,
+        sales.revenue - (sales.quantity * products.purchase_price) as margin
+    
+    from sales
+    inner join products
+        using (products_id)
+)
+
+select * from joined

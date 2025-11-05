fe06ed9ab569985a5af975e4f1367bc5c98a38b1
Gianluca Di Maria <gianlucadimariayt@gmail.com>
2025-11-05 14:40:55 +0000

    Work on operational margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,27 @@
+with 
+
+orders_margin as (
+    select * from {{ ref('int_orders_margin') }}
+),
+
+ship as (
+    select * from {{ ref('stg_raw__ship') }}
+),
+
+joined as (
+    select
+        orders_margin.orders_id,
+        orders_margin.date_date,
+        -- Calcolo operational margin
+        orders_margin.margin 
+            + ship.shipping_fee 
+            - ship.logcost 
+            - ship.ship_cost as operational_margin
+    
+    from orders_margin
+    inner join ship
+        using (orders_id)
+        order by orders_id DESC
+)
+
+select * from joined
